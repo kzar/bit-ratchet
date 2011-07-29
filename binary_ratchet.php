3016b309862d447e16749ef2899267bdc3a5ccf2
David Barker <kzar@kzar.co.uk>
2011-07-29 17:50:54 +0100

    Fixed a few problems.

--- binary_ratchet.php
+++ binary_ratchet.php
@@ -19,23 +19,19 @@ class BinaryRatchet {
   }
 
   public function read($bits) {
-    print "\nCurrent position: " . $this->current_position . "\n";
     // Count number of bytes to read
-    $bytes_needed = $this->round_to_multiple($bits - $this->left_over_bit_count, 8) / 8;
+    $bits_needed = $this->round_to_multiple($bits - $this->left_over_bit_count, 8);
+    $bytes_needed = $bits_needed / 8;
     // Make sure they're avaliable
     if (($bytes_needed * 2 + $current_position) > strlen($this->ascii_hex))
       throw new Exception('BinaryRatchet: read over end of ascii hex.');
     // If so read them
-    print("Bit count: " . $bits . "\n");
-    print("Bytes needed: " . $bytes_needed . "\n");
     $ascii = substr($this->ascii_hex,
                     $this->current_position, $bytes_needed * 2);
-    print("Ascii: " . $ascii . "\n");
     $binary = hexdec($ascii);
     // If there are left over bits add 'em
     if ($this->left_over_bit_count) {
-      $binary = $binary << $this->left_over_bit_count;
-      $binary = $binary | $this->left_over;
+      $binary = $binary | ($this->left_over << $bits_needed);
     }
     // Now trim off any extra bits and save them for later
     $bits_read = ($bytes_needed * 8 + $this->left_over_bit_count);
@@ -43,9 +39,6 @@ class BinaryRatchet {
       $this->left_over_bit_count = $bits_read - $bits;
       $this->left_over = $binary & (pow(2, $this->left_over_bit_count) - 1);
       $binary = $binary >> $this->left_over_bit_count;
-      print "Left over bt count: " . $this->left_over_bit_count . "\n";
-      print "Left over: " . $this->left_over . "\n";
-
     }
     // If not make sure the old left overs are chucked away
     else {
@@ -55,7 +48,6 @@ class BinaryRatchet {
     // Increment our position
     $this->current_position += $bytes_needed * 2;
     // Phew, now return what we've read!
-    print "Result: " . $binary . "\n";
     return $binary;
   }
 }
